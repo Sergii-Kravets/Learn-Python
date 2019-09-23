1b9972ecc50aea2f4b553b7fdafe25c8d66a86ba
Sergii-Kravets <50839786+Sergii-Kravets@users.noreply.github.com>
2019-09-23 19:12:42 +0300

    Create anagram.py

---
+++ Parser-and-statistic/Done-task/anagram/anagram.py
@@ -0,0 +1,20 @@
+"""
+Реализуйте метод, определяющий, является ли одна строка
+перестановкой другой. Под перестановкой понимаем любое
+изменение порядка символов. Регистр учитывается, пробелы
+являются существенными.
+"""
+
+a = 'abaabaab'
+b = 'abaabaab'
+
+
+def is_permutation(a: str, b: str) -> bool:  # первый способ
+    return sorted(a) == sorted(b)
+
+
+print(is_permutation('baba', 'abab'))
+
+print(all(a.count(char) == b.count(char) for char in a))  # второй способ
+
+print(set(a) == set(b))  # третий способ
