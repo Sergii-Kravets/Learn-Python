5d6ccd79451d1a5c919106ce555f2aad7e9eb003
Sergii-Kravets <50839786+Sergii-Kravets@users.noreply.github.com>
2019-09-26 09:34:34 +0300

    Create file

--- 01_Parser-and-statistics/Done-task/carrots/Test.py
+++ 01_Parser-and-statistics/Done-task/carrots/Test.py
@@ -1,17 +1,52 @@
 from collections import Counter
 
-gen = None
-genes = {}
-with open('dna.fasta', 'r') as dna:
-    for data in dna:
-        data = data.strip()
+with open('dna.fasta', 'r') as data:
+    dna_data = data.read().splitlines()
 
-        if '>' in data:
-            gen = data[1:] #name
+
+
+def number_of_nucleotides(data):
+    gen = None
+    genes = {}
+    for dna in data:
+        if '>' in dna:
+            gen = dna[1:]
             genes[gen] = Counter()
         else:
-            for simvol in data:
+            for simvol in dna:
                 genes[gen][simvol] +=1
+    return genes
+
+def dnk_into_rnk (data):
+    rnk = []
+    for counter, str in enumerate(dna_data):
+        if '>' in str:
+            rnk.append(str)
+            continue
+        len_str = len(str)
+        for symbol in str:
+            if symbol == 'A':
+                rnk_symbol = 'T'
+            elif symbol == 'T':
+                rnk_symbol = 'A'
+            elif symbol == 'G':
+                rnk_symbol = 'C'
+            elif symbol == 'C':
+                rnk_symbol = 'G'
+            rnk.append(rnk_symbol)
+
+        all_str = "".join(rnk[counter:len_str + counter])
+        rnk[counter:len_str + counter] = [all_str]
+    return rnk
 
 
-print(genes)
+
+
+
+
+print(dna_data)
+print()
+print(dnk_into_rnk(dna_data))
+
+
+
