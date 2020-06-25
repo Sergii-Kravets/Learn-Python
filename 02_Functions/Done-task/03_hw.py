55267e57993c5c1fd54854b4de816a8d13edc9c9
Sergii-Kravets <50839786+Sergii-Kravets@users.noreply.github.com>
2020-06-25 11:49:31 +0300

    File

--- 02_Functions/Done-task/03_hw.py
+++ 02_Functions/Done-task/03_hw.py
@@ -1,8 +1,43 @@
-Напишите реализацию функции make_it_count, которая принимает в качестве
+"""Напишите реализацию функции make_it_count, которая принимает в качестве
 аргументов некую функцию (обозначим ее func) и имя глобальной переменной
 (обозначим её counter_name), возвращая новую функцию, которая ведет себя
 в точности как функция func, за тем исключением, что всякий раз при вызове
 инкрементирует значение глобальной переменной с именем counter_name.
+"""
+
+counter_name = 0
 
 
-decorator
+def make_it_count(func):
+    global counter_name
+
+    same = func
+
+    def same_func():
+        global counter_name
+        counter_name += 1
+        return func()
+
+    return same_func
+
+
+def func():
+    return 1 + 1
+
+
+print(func())
+
+print(make_it_count(func))
+
+a = make_it_count(func)
+
+print(a())
+
+a()
+a()
+a()
+a()
+a()
+a()
+
+print(counter_name)
--- 02_Functions/Done-task/04_hw.py
+++ 02_Functions/Done-task/04_hw.py
@@ -1,3 +1,4 @@
+"""
 Напишите функцию modified_func, которая принимает функцию (обозначим ее func),
 а также произвольный набор позиционных (назовем их fixated_args) и именованных
 (назовем их fixated_kwargs) аргументов и возвращает новую функцию,
@@ -12,18 +13,36 @@ fixed_args (приписывает в конец списка fixated_args), и 
 3.Имеет __name__ вида func_<имя функции func>
 4.Имеет docstring вида:
 
-"""
 A func implementation of <имя функции func>
 with pre-applied arguments being:
 <перечисление имен и значений fixated_args и fixated_kwargs>
 source_code:
    ...
-"""
+
 
 Для того, чтобы получить имена позиционных аргументов и исходный код, советуем использовать
 возможности модуля inspect.
 
 Попробуйте применить эту функцию на написанных функциях из дз1, дз2, дз3. К функциям min, max, any() ?
-
-
 decorator
+"""
+
+def func(*args, **kwargs):
+    return print(args, kwargs)
+
+
+def partial(func, *fixated_args, **fixated_kwargs):
+    def new_func(*new_func_args, **new_func_kwargs):
+        if new_func_args and new_func_kwargs:
+            fixated_kwargs.update(**new_func_kwargs)
+            return func(*fixated_args.__add__(new_func_args), **fixated_kwargs)
+        else:
+            return func(*fixated_args, **fixated_kwargs)
+
+    return new_func
+
+
+example_func = partial(func, 2, 2, 2, **{"very_very_nice": 222})
+
+example_func(33, 33, 33, **{"very_nice": 333444})
+example_func(33, 33123, 33, **{"very_very_nice": 0})
